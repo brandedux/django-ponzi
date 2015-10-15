99e9e87f5493f5a35ab6d3312331f5f074b8a8ce
Spencer Dupre <spencer.dupre@gmail.com>
2015-10-15 18:05:56 -0400

    Added setuptools

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-ponzi',
+    version='0.1',
+    packages=['ponzi'],
+    include_package_data=True,
+    license='GNU GENERAL PUBLIC LICENSE VERSION 3',
+    description='A cryptocurrency ponzi app for Django.',
+    long_description=README,
+    url='https://github.com/realspencerdupre/django-ponzi',
+    author='Spencer Dupre',
+    author_email='spencer.dupre@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE VERSION 3', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
