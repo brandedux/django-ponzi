0ed4f12e9bbb80d54aa33be0b50397ee7f8ea237
Spencer Dupre <spencer.dupre@gmail.com>
2015-10-17 11:36:42 -0400

    Removed static index page

--- ponzi/urls.py
+++ ponzi/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = patterns(
     'ponzi.views',
     url(r'^$', views.index, name='index'),
-    url(r'^index.html$', views.simple_index, name='simple_index'),
     url(r'^add/$', views.add, name='add'),
     url(r'^list/$', views.addr_list, name='addr_list'),
     url(r'^callback/$', views.callback, name='callback'),
